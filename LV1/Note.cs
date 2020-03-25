854e7be0aace89a042b9a99ae4080f76509d33b8
lordbabo <62650991+lordbabo@users.noreply.github.com>
2020-03-25 17:53:52 +0100

    Update Note.cs

--- LV1/Note.cs
+++ LV1/Note.cs
@@ -8,15 +8,52 @@ namespace LV1
 {
     class Note
     {
-        private String text { get; private set; }
-        private String author { get; set; }
-        private int notesPriority { get; set; }
+        private String text;
+        private String author;
+        private int notesPriority;
         public Note()
         {
             this.text = "Default Text";
             this.author = "Default Author";
             this.notesPriority = 1;
         }
+        
+        public string Text
+        {
+            get { return this.text; }
+            set { this.text = value; }
+        }
+        public string Author
+        {
+            get { return this.author; }
+            private set { this.author = value; }
+        }
+        public int NotesPriority
+        {
+            get{ return this.notesPriority; }
+            set { this.priority = value; }
+        }
+        public String getText()
+        {
+            return this.text;
+        }
+        public String getAuthor()
+        {
+            return this.author;
+        }
+        public int getNotesPriority()
+        {
+            return this.priority;
+        }
+        public void setText(String text)
+        {
+            this.text = text;
+        }
+        public void setPriority(int notesPriority)
+        {
+            this.notesPriority = notesPriority;
+        }
+
         public Note(string text)
         {
             this.text = text;
